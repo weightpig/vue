695c2ebac85cdb3e4517ecfa36ae01b5e7fed9aa
chenying <chenying@uniontech.com>
2020-04-26 17:46:58 +0800

    添加处理路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Shouye from '@/components/Shouye/Shouye'
+import Bankuai from '@/components/Bankuai/Bankuai'
+import Zhuti from '@/components/Zhuti/Zhuti'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/zhuti',
+      name: 'Zhuti',
+      component: Zhuti
+    },{
+      path: '/bankuai',
+      name: 'Bankuai',
+      component: Bankuai
+    },
     {
       path: '/',
       name: 'Shouye',
